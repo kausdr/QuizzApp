d7ed40725356d2679932cbb933c90bdd43efba4a
Kauane <86927114+kausdr@users.noreply.github.com>
2024-11-05 08:04:36 -0300

    adds image to the code

--- app/src/main/java/my/app/quizsomativo/Quizz.kt
+++ app/src/main/java/my/app/quizsomativo/Quizz.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -15,11 +17,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import my.app.quizsomativo.ui.theme.QuizSomativoTheme
@@ -43,6 +48,12 @@ fun TextQuestion(text: String, modifier: Modifier = Modifier, style: androidx.co
 }
 
 @Composable
+fun listQuetion() {
+    val questions = listOf(Pair("Olivia Rodrigo",R.drawable.or), Pair("Bruno Mars",R.drawable.bruno_mars), Pair("Alcione",R.drawable.alcione))
+}
+
+
+@Composable
 fun TextAnswer(text: String, modifier: Modifier = Modifier, style: androidx.compose.ui.text.TextStyle) {
     Text(text = "$text",modifier = modifier, style = style)
 
@@ -63,6 +74,7 @@ fun RadioAnswer(option: String, selectedOption: String?, onOptionSelected: (Stri
 fun QuizzGameplayUI() {
     var textQuestion by remember { mutableStateOf("Quem foi michael jackson, mais conhecido como Lord?") }
     val options1 = listOf("Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4")
+    val questions = listOf(Pair("Olivia Rodrigo",R.drawable.or), Pair("Bruno Mars",R.drawable.bruno_mars), Pair("Alcione",R.drawable.alcione))
     var selectedOption by remember { mutableStateOf<String?>(null) }
 
         QuizSomativoTheme {
@@ -74,6 +86,21 @@ fun QuizzGameplayUI() {
                     )
                 )
 
+                questions.forEach { (question, img) ->
+
+                    Row(modifier = Modifier
+                        .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ) {
+                        TextQuestion(text = question, style = androidx.compose.ui.text.TextStyle())
+                        Image(
+                            painter = painterResource(id = img),
+                            contentDescription = ""
+                        )
+                    }
+                }
+
                 options1.forEach { option ->
 
                     Row(modifier = Modifier
@@ -81,7 +108,6 @@ fun QuizzGameplayUI() {
                             verticalAlignment = Alignment.CenterVertically
 
                     ) {
-
                         RadioAnswer(
                             option = option,
                             selectedOption = selectedOption,
@@ -91,6 +117,8 @@ fun QuizzGameplayUI() {
                     }
                 }
             }
+
+
 
         }
     }
